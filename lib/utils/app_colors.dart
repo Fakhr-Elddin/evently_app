a2d94c42a8903edb4759bd37db020d61cbfc916a
Fakhr-Elddin <fakhereldeanaymanq@gmail.com>
2026-03-25 20:19:53 +0200

    create app colors file

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColorLight = Color(0xff5669FF);
+  static const backgroundColorLight = Color(0xffF2FEFF);
+  static const blackColor = Color(0xff1C1C1C);
+  static const whiteColor = Color(0xffFFFFFF);
+  static const greyColor = Color(0xff7B7B7B);
+  static const redColor = Color(0xffFF5659);
+}
